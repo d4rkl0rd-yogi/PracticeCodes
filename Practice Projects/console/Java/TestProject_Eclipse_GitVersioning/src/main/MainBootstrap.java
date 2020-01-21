81d172cb0b5519251d4904ed3213943936c6fc11
d4rkl0rd-yogi <60072062+d4rkl0rd-yogi@users.noreply.github.com>
2020-01-22 02:34:47 +0530

    Commiting this project to test this Git repo structure operation and deploying the project to Git repo

---
+++ Practice Projects/console/Java/TestProject_Eclipse_GitVersioning/src/main/MainBootstrap.java
@@ -0,0 +1,8 @@
+package main;
+
+public class MainBootstrap {
+	public static void main(String[] args) {
+		System.out.println("This is a test project to test the "
+				+ "project deployment to the GIT repository");
+	}
+}
